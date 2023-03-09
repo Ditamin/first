0bf13157fbb866bc539bdadcac5c6fe67aa43b6f
Ditamin <125723968+Ditamin@users.noreply.github.com>
2023-03-10 00:28:21 +0500

    Update rational.h

--- rational/rational.h
+++ rational/rational.h
@@ -1,23 +1,22 @@
 #ifndef RATIONAL_H
 #define RATIONAL_H
-#include <iostream>
 
-int64_t GCD(int64_t a, int64_t b);
+int GCD(int a, int b);
 
 class RationalDivisionByZero : public std::exception {};
 
 class Rational {
- private:
-  int64_t p_;
-  int64_t q_;
-
- public:
-  Rational(int64_t p = 0, int64_t q = 1);  //  NOLINT
+public:
+  Rational(int p = 0, int q = 1);  //  NOLINT
   Rational(const Rational& other);
-  int64_t GetNumerator() const;
-  void SetNumerator(int64_t p);
-  int64_t GetDenominator() const;
-  void SetDenominator(int64_t q);
+  int GetNumerator() const;
+  int GetDenominator() const;
+  void SetNumerator(int p);
+  void SetDenominator(int q);
+
+private:
+  int p_;
+  int q_;
   void Reduce();
 };
 
@@ -25,22 +24,27 @@ Rational& operator+=(Rational& first, const Rational& second);
 Rational& operator-=(Rational& first, const Rational& second);
 Rational& operator*=(Rational& first, const Rational& second);
 Rational& operator/=(Rational& first, const Rational& second);
+
 Rational& operator++(Rational& a);
-Rational operator++(Rational& a, int);
 Rational& operator--(Rational& a);
+Rational operator++(Rational& a, int);
 Rational operator--(Rational& a, int);
 Rational operator+(Rational a);
 Rational operator-(Rational a);
+
 Rational operator+(const Rational& first, const Rational& second);
 Rational operator-(const Rational& first, const Rational& second);
 Rational operator*(const Rational& first, const Rational& second);
 Rational operator/(const Rational& first, const Rational& second);
+
 bool operator<(const Rational& first, const Rational& second);
 bool operator>(const Rational& first, const Rational& second);
 bool operator==(const Rational& first, const Rational& second);
 bool operator!=(const Rational& first, const Rational& second);
 bool operator<=(const Rational& first, const Rational& second);
 bool operator>=(const Rational& first, const Rational& second);
+
 std::istream& operator>>(std::istream& is, Rational& val);
 std::ostream& operator<<(std::ostream& os, const Rational& val);
+
 #endif
