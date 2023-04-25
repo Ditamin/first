bd1dc1b4625cf4bb683231de470dc69376dfea72
Ditamin <125723968+Ditamin@users.noreply.github.com>
2023-04-25 19:50:45 +0500

    Update string_view.h

--- string_view/string_view.h
+++ string_view/string_view.h
@@ -4,31 +4,34 @@
 #include <iostream>
 
 class StringViewOutOfRange : public std::out_of_range {
- public:
+public:
   StringViewOutOfRange() : std::out_of_range("StringViewOutOfRange") {
   }
 };
 
 class StringView {
- public:
+public:
   StringView() = default;
   StringView(const char*);  // NOLINT
   StringView(const char*, size_t);
+
   const char& operator[](size_t) const;
   const char& At(size_t) const;
   const char& Front() const;
   const char& Back() const;
+
   size_t Size() const;
   size_t Length() const;
   bool Empty() const;
   const char* Data() const;
+
   void Swap(StringView&);
   void RemovePrefix(size_t);
   void RemoveSuffix(size_t);
   StringView Substr(size_t, size_t);
 
- private:
-  const char* sv_ = nullptr;
+private:
+  const char* str_ = nullptr;
   size_t size_ = 0;
 };
 
